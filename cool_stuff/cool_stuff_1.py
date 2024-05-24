e3e8a1049b841087950dea790cca91d439e838b3
andyatterton <andrew.atterton@ukkidney.org>
2024-05-24 13:26:11 +0100

    thing

--- cool_stuff/cool_stuff_1.py
+++ cool_stuff/cool_stuff_1.py
@@ -4,13 +4,14 @@ from rr_connection_manager import PostgresConnection, SQLServerConnection
 conn = PostgresConnection(app="radar_live", tunnel=True)
 # conn = SQLServerConnection(app='renalreg_live')
 
-conn.connection_check()
 
 
 
 
 
 
+if __name__ == '__main__':
+    conn.connection_check()
 
 
 
